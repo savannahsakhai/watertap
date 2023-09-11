9b7ee1ea6834b3a661f432604a2e626400bbbcdb
savannahsakhai <sss0031@mix.wvu.edu>
2023-09-11 09:41:11 -0400

    simple_prop_pack and stateblock sweep

--- watertap/examples/flowsheets/property_comparison/State Block/Stateblock_Sweep.py
+++ watertap/examples/flowsheets/property_comparison/State Block/Stateblock_Sweep.py
@@ -1,33 +1,55 @@
 import NaCl_Stateblock as flowsheet_NaCl
 import Sea_Stateblock as flowsheet_Sea
+import Simple_Stateblock as flowsheet_simple
 from watertap.tools.parameter_sweep import parameter_sweep, LinearSample
 
 
-# --------------------- Seawater Stateblock ---------------------
+# # --------------------- Seawater Stateblock ---------------------
 
-m = flowsheet_Sea.main()
+# m = flowsheet_Sea.main()
 
-sweep_params = dict()
-sweep_params["Temperature"] = LinearSample(
-    m.fs.stream[0].temperature, 20 + 273.15, 150 + 273.15, 25
-)
-sweep_params["Flow"] = LinearSample(
-    m.fs.stream[0].flow_mass_phase_comp["Liq", "TDS"], 0.01, 0.26, 15
-)
+# sweep_params = dict()
+# sweep_params["Temperature"] = LinearSample(
+#     m.fs.stream[0].temperature, 20 + 273.15, 150 + 273.15, 25
+# )
+# sweep_params["Flow"] = LinearSample(
+#     m.fs.stream[0].flow_mass_phase_comp["Liq", "TDS"], 0.01, 0.26, 15
+# )
 
-outputs = dict()
-outputs["Enthalpy"] = m.fs.stream[0].enth_mass_phase["Liq"]
-outputs["Vap Pressure"] = m.fs.stream[0].pressure_sat
-outputs["Cp"] = m.fs.stream[0].cp_mass_phase["Liq"]
+# outputs = dict()
+# outputs["Enthalpy"] = m.fs.stream[0].enth_mass_phase["Liq"]
+# outputs["Vap Pressure"] = m.fs.stream[0].pressure_sat
+# outputs["Cp"] = m.fs.stream[0].cp_mass_phase["Liq"]
 
-parameter_sweep(
-    m, sweep_params, outputs, csv_results_file_name="outputs_results_Sea.csv"
-)
+# parameter_sweep(
+#     m, sweep_params, outputs, csv_results_file_name="outputs_results_Sea.csv"
+# )
 
 
-# --------------------- NaCl Stateblock ---------------------
+# # --------------------- NaCl Stateblock ---------------------
 
-m = flowsheet_NaCl.main()
+# m = flowsheet_NaCl.main()
+
+# sweep_params = dict()
+# sweep_params["Temperature"] = LinearSample(
+#     m.fs.stream[0].temperature, 20 + 273.15, 150 + 273.15, 25
+# )
+# sweep_params["Flow"] = LinearSample(
+#     m.fs.stream[0].flow_mass_phase_comp["Liq", "NaCl"], 0.01, 0.26, 15
+# )
+
+# outputs = dict()
+# outputs["Enthalpy"] = m.fs.stream[0].enth_mass_phase["Liq"]
+# outputs["Vap Pressure"] = m.fs.stream[0].pressure_sat
+# outputs["Cp"] = m.fs.stream[0].cp_mass_phase["Liq"]
+
+# parameter_sweep(
+#     m, sweep_params, outputs, csv_results_file_name="outputs_results_NaCl.csv"
+# )
+
+# --------------------- Simple Stateblock ---------------------
+
+m = flowsheet_simple.main()
 
 sweep_params = dict()
 sweep_params["Temperature"] = LinearSample(
@@ -40,8 +62,8 @@ sweep_params["Flow"] = LinearSample(
 outputs = dict()
 outputs["Enthalpy"] = m.fs.stream[0].enth_mass_phase["Liq"]
 outputs["Vap Pressure"] = m.fs.stream[0].pressure_sat
-outputs["Cp"] = m.fs.stream[0].cp_mass_phase["Liq"]
+# outputs["Cp"] = m.fs.stream[0].cp_mass_phase["Liq"]
 
 parameter_sweep(
-    m, sweep_params, outputs, csv_results_file_name="outputs_results_NaCl.csv"
+    m, sweep_params, outputs, csv_results_file_name="outputs_results_simple.csv"
 )
